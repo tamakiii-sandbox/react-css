1d2cb269ba81ce877fbb06014145cab2d9b6e934
tamakiii <tamakiii@users.noreply.github.com>
2019-06-17 06:52:32 +0900

    add postcss-plugins

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   plugins: [
     require('autoprefixer'),
-    require('postcss-nested')
+    require('postcss-preset-env'),
+    require('postcss-nested'),
+    require('postcss-sorting'),
   ]
 }
